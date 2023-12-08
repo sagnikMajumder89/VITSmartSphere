dfc0a97af46ef8887d68ab2564ac02495786832d
sagnikMajumder89 <sagnikm183@gmail.com>
2023-12-08 17:05:05 +0530

    fix in fetch req

--- client/src/components/Authentication/login.jsx
+++ client/src/components/Authentication/login.jsx
@@ -24,7 +24,7 @@ export default function Login() {
             })
     }
     const handleLogin = async () => {
-        const res = await fetch('http://localhost:3000/auth/login', {
+        const res = await fetch('https://smartspherevit.onrender.com/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
